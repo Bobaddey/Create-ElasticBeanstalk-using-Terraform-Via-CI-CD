cea81ee68cdaad3f6565c18e8e78b96255a81414
Bobaddey <addeybob@gmail.com>
2024-03-12 12:16:14 +0100

    backend and workflow

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-ebs"
-    key    = "/"
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 #This is to configure the aws credentials to authenticate to the AWS account
 provider "aws" {
   region = "us-east-1"
-  access_key = ${{ secrets.access_key }}
-  secret_key = ${{ secrets.sceret_key }}
+  access_key = ${{ secrets.ACCESS_KEY }}
+  secret_key = ${{ secrets.SECRET_KEY }}
 }
