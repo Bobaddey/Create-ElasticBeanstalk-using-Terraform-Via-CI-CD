02cf7bcbea39325dcc16b842b2482826a579fa0b
Denzel001 <denzel.obioha@lendistry.com>
2024-03-12 13:09:07 +0100

    terraform pipeline

--- provider.tf
+++ provider.tf
@@ -1,6 +1,4 @@
 #This is to configure the aws credentials to authenticate to the AWS account
 provider "aws" {
   region = "us-east-1"
-  access_key = "${{ secrets.ACCESS_KEY }}"
-  secret_key = "${{ secrets.SECRET_KEY }}"
 }
